deca54c82f2c1a89c48a9fd20f43ac9b5ceaadd0
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-10-31 20:08:21 +0530

    working on navbar

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+//nykaa dhamaka
+let nykaa_dhamaka = document.getElementById("nykaa-dhamaka")
+let flag = true
+setInterval(() => {
+
+    if(flag == true){
+        nykaa_dhamaka.style.opacity = "0"
+        flag = false
+    }else {
+        nykaa_dhamaka.style.opacity = "1"
+        flag = true
+    }
+},2500)
+
+//nykaa dhamaka end
+
+//brand section navbar
+
+let BRAND = document.querySelector(".show-brandbox")
+BRAND.addEventListener("mouseover" , ShowBrandBox)
+BRAND.addEventListener("mouseleave" , HideBOxes1)
+
+let brand_box = document.querySelector(".brand-box")
+brand_box.addEventListener("mouseleave" , HideBrandBox)
+
+function ShowBrandBox() {
+    brand_box.style.display = "grid"
+}
+function HideBrandBox() {
+    brand_box.style.display = "none"
+}
+
+function HideBOxes1() {
+
+}
+
+let popular_btn = document.querySelector(".popular-btn")
+popular_btn.addEventListener("mouseover" , showPopularBrand)
+
+let brand_popular_img = document.querySelector(".brand-imagesherepopular")
+let brand_featured_img = document.querySelector(".brand-imageshereFeatured")
+
+function showPopularBrand() {
+    brand_popular_img.style.display = "grid"
+    brand_featured_img.style.display = "none"
+}
+
+let featured_btn = document.querySelector(".Featured-btn")
+featured_btn.addEventListener("mouseover" , ShowFeaturedimg)
+
+function ShowFeaturedimg() {
+    brand_popular_img.style.display = "none"
+    brand_featured_img.style.display = "grid"
+}
+
+let onlyat_btn = document.querySelector(".onlynykaa")
+onlyat_btn.addEventListener("mouseover" , showPopularBrand)
+
+let new_launches = document.querySelector(".newlaunches")
+new_launches.addEventListener("mouseover" , ShowFeaturedimg)
