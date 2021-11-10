c6cec9942d69a5e5c956768873f98ac65522dfe1
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2021-11-10 17:21:55 +0530

    address script finishing remaining

--- scripts/address.js
+++ scripts/address.js
@@ -12,3 +12,43 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+let ship = document.getElementById("ship");
+ship.addEventListener("click",()=>{
+    delivery();
+})
+
+function delivery()
+{
+    let name  = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let pc = document.getElementById("pc").value;
+    let add = document.getElementById("add").value;
+    console.log(name,phone,pc,add);
+
+    let userdetail = {
+        name,
+        phone ,
+        pc,
+        add,
+        
+    };
+    if (localStorage.getItem("addressuser") === null) {
+        localStorage.setItem("addressuser", JSON.stringify([]));
+    }
+
+    let addressl = JSON.parse(localStorage.getItem("addressuser"));
+    console.log(addressl);
+    addressl.push(userdetail);
+    console.log(userdetail)
+    localStorage.setItem("addressuser", JSON.stringify(addressl));
+
+
+    name.value = "";
+    phone.value = "";
+    pc.value = "";
+    add.value = "";
+    
+    window.location.href = "payment.html";
+}
+
