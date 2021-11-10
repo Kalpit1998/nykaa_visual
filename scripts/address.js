cb43d56d5fcbc39e6146061cde66d32859f28eb2
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-11-11 00:53:49 +0530

    Address page is done with complete functionality.
    Address page is done with complete functionality.

--- scripts/address.js
+++ scripts/address.js
@@ -1,3 +1,127 @@
+
+let smallemail_ = document.querySelector(".smallemail")
+
+if(localStorage.getItem("userData") === null) {
+  localStorage.setItem("userData", JSON.stringify([]))
+}
+
+let getuserData = JSON.parse(localStorage.getItem("userData"))
+
+smallemail_.textContent = getuserData[getuserData.length-1].email
+
+localStorage.setItem("userData" , JSON.stringify(getuserData))
+
+
+
+let savedADDRESSES = document.querySelector(".savedADDRESSES")
+let newADDRESSES = document.querySelector(".newADDRESSES")
+let newaddress_box = document.querySelector("#newaddress-box")
+let saved_addresses_cont = document.querySelector(".saved-addresses-cont")
+let add_edit_btn = document.querySelector(".add-edit-btn")
+
+newADDRESSES.addEventListener("click" , fnnewADDRESSES)
+savedADDRESSES.addEventListener("click" , fnsavedADDRESSES)
+add_edit_btn.addEventListener("click" , ShownewADDRESSE)
+
+function fnnewADDRESSES() {
+    saved_addresses_cont.style.display = "none"
+    savedADDRESSES.style.borderRight = "none"
+    newADDRESSES.style.borderRight = "3px solid #fc2779"
+    newaddress_box.style.display = "flex"
+}
+
+function fnsavedADDRESSES() {
+  saved_addresses_cont.style.display = "block"
+  savedADDRESSES.style.borderRight = "3px solid #fc2779"
+  newADDRESSES.style.borderRight = "none"
+  newaddress_box.style.display = "none"
+}
+
+function ShownewADDRESSE() {
+  saved_addresses_cont.style.display = "none"
+  savedADDRESSES.style.borderRight = "none"
+  newADDRESSES.style.borderRight = "3px solid #fc2779"
+  newaddress_box.style.display = "flex"
+}
+
+let cartiteam = document.getElementById("cartiteam")
+
+let getNykaaCArt = JSON.parse(localStorage.getItem("NykaaCart")) 
+
+
+let number__cart = document.querySelector(".number__cart")
+number__cart.textContent = getNykaaCArt.length
+
+
+
+function AppendtoCartItem(getNykaaCArt) {
+cartiteam.innerHTML = null
+getNykaaCArt.forEach((prod) => {
+
+  let maindiv = document.createElement("div")
+  maindiv.setAttribute("class" , "main__div")
+
+  let img_div = document.createElement("div")
+  img_div.setAttribute("class" , "cartimg_div")
+
+  let text_div = document.createElement("div")
+  text_div.setAttribute("class" , "text__div")
+
+  let img = document.createElement("img")
+  img.src = prod.img1
+
+  img_div.append(img)
+
+  let pname = document.createElement("p")
+  pname.setAttribute("class" , "Pname")
+  pname.textContent = prod.name.substring(0,30)
+
+  let qpdiv = document.createElement("div")
+  qpdiv.setAttribute("class" , "qpdiv")
+
+  let Qtyp = document.createElement("p")
+  Qtyp.innerText = "Qty : 1"
+
+
+  let pri = document.createElement("p")
+  pri.textContent = "₹" + prod.price
+
+  qpdiv.append(Qtyp, pri)
+
+  text_div.append(pname, qpdiv)
+
+  maindiv.append(img_div, text_div)
+
+  cartiteam.append(maindiv)
+})
+
+}
+AppendtoCartItem(getNykaaCArt)
+
+localStorage.setItem("NykaaCart" , JSON.stringify(getNykaaCArt))
+
+
+let NYkaaTotal = JSON.parse(localStorage.getItem("NykaaTotal"))
+
+let subt = document.querySelector("#subt")
+subt.textContent = "₹" + NYkaaTotal[NYkaaTotal.length-1]
+
+let usrewt = document.getElementById("usrewt")
+
+usrewt.textContent = "₹" + NYkaaTotal[NYkaaTotal.length-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -13,6 +137,13 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+if (localStorage.getItem("MyNykaaAddress") === null) {
+  localStorage.setItem("MyNykaaAddress", JSON.stringify([]));
+}
+
+let redirect_paybtn = document.querySelector(".redirect_paybtn")
+redirect_paybtn.addEventListener("click" , delivery)
+
 let ship = document.getElementById("ship");
 ship.addEventListener("click",()=>{
     delivery();
@@ -24,24 +155,24 @@ function delivery()
     let phone = document.getElementById("phone").value;
     let pc = document.getElementById("pc").value;
     let add = document.getElementById("add").value;
-    console.log(name,phone,pc,add);
 
     let userdetail = {
-        name,
-        phone ,
-        pc,
-        add,
-        
+        Address: add,
+        MobileNo : phone,
+        Postel : pc,
+        name : name 
     };
-    if (localStorage.getItem("addressuser") === null) {
-        localStorage.setItem("addressuser", JSON.stringify([]));
+   
+
+    let addressl = JSON.parse(localStorage.getItem("MyNykaaAddress"));
+
+    if(name != ""  &&  phone != ""  &&  pc != ""  &&  add != "") {
+
+    addressl.push(userdetail);
+    
     }
 
-    let addressl = JSON.parse(localStorage.getItem("addressuser"));
-    console.log(addressl);
-    addressl.push(userdetail);
-    console.log(userdetail)
-    localStorage.setItem("addressuser", JSON.stringify(addressl));
+    localStorage.setItem("MyNykaaAddress", JSON.stringify(addressl));
 
 
     name.value = "";
